ccda7ac249448154b80d1b0525b5eda9301a5c08
WilliamsD2018 <dwilliams@sjnma.org>
2018-03-01 09:30:05 -0600

    homework

--- hw5/problem2.py
+++ hw5/problem2.py
@@ -1,2 +1,7 @@
-variable = raw_input("Enter a number")
- 
+variable = float (raw_input ("Enter a number"))
+if (variable%2 == 0):
+	Number = (variable + 1)* 3.0
+	print (Number)
+else:
+        Number = variable/2.0
+	print (Number) 
