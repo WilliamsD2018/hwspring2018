5e1408674ff98e11f09292e6a0730e8f2be0a729
WilliamsD2018 <dwilliams@sjnma.org>
2018-03-19 20:16:51 -0500

    Hello

--- hw7/Problem-1.py
+++ hw7/Problem-1.py
@@ -1,5 +1,4 @@
 def uppercasefinder(sentence)
-	words = []
 	
 	return words
 upperWords = uppercasefinder("this is A VERY INTRESTING way of FINDING words")
---
+++ hw7/Problem-2.py
@@ -0,0 +1,7 @@
+#Problem 2
+import csv
+with open('roster.csv' , 'rb') as f:
+	reader = csv.reader(f)
+	for row in reader:
+		print row
+
