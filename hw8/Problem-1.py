b9d6c0ddadb97505a7c56e432ba2b7c5c7852011
WilliamsD2018 <dwilliams@sjnma.org>
2018-04-11 09:29:10 -0500

    Hw8

---
+++ hw8/Problem-1.py
@@ -0,0 +1,24 @@
+import random
+user1 = 0
+user2 = 0
+
+while user1 < 2 and user2 < 2:
+	turn1 = random.randint(1,6)
+	turn2 = random.randint(1,6)
+	if turn1 > turn2:
+		print("user 1 is the winner of the round")
+		user1 += 1
+		print(turn1)
+	elif turn2 > turn1:
+		print("user 2 is the winner of the round")
+		user2 +=1
+		print(turn2)
+	elif turn1 == turn2:
+		print("Draw")
+
+print("Result")
+print(user1, user2)
+if user1 > user2:
+	print("user1 wins")
+elif(user2 > user1):
+	print("user2 wins")
